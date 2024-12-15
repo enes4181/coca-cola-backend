2807bad88976a13dc1f0946d852d9b24f9e5a80a
Enes Copoglu <copoglu.81@gmail.com>
2024-12-15 17:02:50 +0300

    whiteList instert

--- backend/src/helpers/corsOptions.js
+++ backend/src/helpers/corsOptions.js
@@ -1,4 +1,4 @@
-const whiteList = ["http://localhost:3000"]
+const whiteList = ["http://localhost:3000", "https://coca-cola-products.netlify.app"]
 // daha fazla varsa "", ile devam ettir
 
 const corsOptions = (req, callback) => {
